6a0b5eec03ed9084a954397c7b3bf2dfcc3bcfd5
als79gur49 <alsgur7949@naver.com>
2024-09-26 00:12:15 +0900

    merge테스트

--- Assets/Scripts/TestUploadGitHub.cs
+++ Assets/Scripts/TestUploadGitHub.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class TestUploadGitHub : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public Vector3 testVector;
+
     void Start()
     {
         
