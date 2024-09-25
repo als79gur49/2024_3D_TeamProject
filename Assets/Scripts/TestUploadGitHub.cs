1fbd5e5da8f1342f9bb4f62277209a70456cfc7e
als79gur49 <67832660+als79gur49@users.noreply.github.com>
2024-09-26 00:14:45 +0900

    머지 테스트

--- Assets/Scripts/TestUploadGitHub.cs
+++ Assets/Scripts/TestUploadGitHub.cs
@@ -7,7 +7,7 @@ public class TestUploadGitHub : MonoBehaviour
     // Start is called before the first frame update
 
     public Vector3 testVector;
-
+    public Vector3 test;
     void Start()
     {
         
